1fec19dabb8d3be071f254e1d9437c5befc3e8d3
JEANYPC\Gebruiker <jeanydevries@hotmail.com>
2020-08-24 11:04:17 +0200

    Project added

---
+++ RollaBall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 0;
+    [SerializeField] int winRate;
+    [SerializeField] TextMeshProUGUI countText;
+    [SerializeField] GameObject winTextObject;
+
+    private Rigidbody rb;
+
+    private float movementX;
+    private float movementY;
+
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        count = 0;
+
+        SetCountText();
+
+        winTextObject.SetActive(false);
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= winRate)
+        {
+            winTextObject.SetActive(true);
+            Time.timeScale = 0.2f;
+        }
+    }
+}
